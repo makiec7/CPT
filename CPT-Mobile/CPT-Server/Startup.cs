a01070e708be0af410a60866630c0b8cd7e1c3ee
Maciej Adamiak <180122@edu.p.lodz.pl>
2016-10-17 18:10:31 +0200

    Project name changed

---
+++ CPT-Mobile/CPT-Server/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CPT_Server.Startup))]
+namespace CPT_Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
